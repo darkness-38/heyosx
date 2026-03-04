543f6af5bf06f5c823fbf99d16ac086405d269fc
YourDarkestFear <someoneteachs@gmail.com>
2026-03-04 20:45:01 +0300

    feat(02-02): update server.h for XDG-shell

--- src/server.h
+++ src/server.h
@@ -7,6 +7,8 @@
 #include <wlr/render/wlr_renderer.h>
 #include <wlr/types/wlr_scene.h>
 #include <wlr/types/wlr_output_layout.h>
+#include <wlr/types/wlr_output.h>
+#include <wlr/types/wlr_xdg_shell.h>
 
 struct heyde_server {
     struct wl_display *wl_display;
@@ -15,6 +17,36 @@ struct heyde_server {
     struct wlr_allocator *allocator;
     struct wlr_scene *scene;
     struct wlr_output_layout *output_layout;
+
+    struct wl_list outputs;
+    struct wl_listener new_output;
+
+    struct wlr_xdg_shell *xdg_shell;
+    struct wl_list toplevels;
+    struct wl_listener new_xdg_surface;
+};
+
+struct heyde_output {
+    struct wl_list link;
+    struct heyde_server *server;
+    struct wlr_output *wlr_output;
+    struct wl_listener frame;
+    struct wl_listener request_state;
+    struct wl_listener destroy;
+};
+
+struct heyde_toplevel {
+    struct wl_list link;
+    struct heyde_server *server;
+    struct wlr_xdg_toplevel *xdg_toplevel;
+    struct wlr_scene_tree *scene_tree;
+    struct wl_listener map;
+    struct wl_listener unmap;
+    struct wl_listener destroy;
+    struct wl_listener request_move;
+    struct wl_listener request_resize;
+    struct wl_listener request_maximize;
+    struct wl_listener request_fullscreen;
 };
 
 #endif
